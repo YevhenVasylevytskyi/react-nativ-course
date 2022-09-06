abb04a7cf8eb0c190ec313492e6d3c119ead5f25
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-09-07 01:39:50 +0300

    Login screen in progress

--- App.js
+++ App.js
@@ -1,23 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import LoginScreen from './Screens/LoginScreen.js';
-// import RegistrationScreen from './Screens/RegistrationScreen';
+import {
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { useState} from 'react'
 
 export default function App() {
+  const [loginValue, setLoginValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
   return (
+    
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
-      {/* <LoginScreen /> */}
-      <StatusBar style="auto" />
-    </View>
+      <ImageBackground
+        style={styles.backgraundImage}
+        source={require('./assets/images/backgraund-image.jpg')}
+      > 
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <Text>Увійти</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Login"
+              value={loginValue}
+              // onChangeText={inputHandler}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              value={passwordValue}
+              // onChangeText={inputHandler}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              // onPress={onPress}
+            >
+              <Text
+                style={styles.buttomTitle}
+              >
+                Увійти
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.regText}
+            >
+              Немає аккаунуту? Зареєструватись
+            </Text>
+        </View>
+        </View>  
+        <StatusBar style="auto" />
+      </ImageBackground>
+    </View>    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  backgraundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end'
+  },
+  form: {
+    // alignItems: 'center',
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    
+  },
+  content: {
+    
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  textInput: {
+    borderRadius : 10,
+    borderColor: 'red',
+    alignItems: "center",
+    justifyContent: "center",
+
+    
+    padding: 5,
+    backgroundColor: '#fff'
+
+  },
+  button: {
+    alignItems: "center",
+    marginHorizontal: 16,
+    borderRadius: 100,
+    // height: 32,
+    // weigth: 100,
+    paddingTop: 16,
+    paddingBottom: 16,
+    backgroundColor: '#FF6C00',
+    marginBottom: 16,
+  },
+  buttomTitle: {    
+    color:'#fff',
+  },
+  regText: {
+    
+    marginBottom: 144,  
+  }
 });
